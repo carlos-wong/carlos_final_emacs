f43072bbdfa2437a72890791f77046861aef24b7
carlos <huaixian.huang@gmail.com>
2018-06-12 23:02:46 +0800

    Feat: use-package work

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,33 @@
-(message "hicarlos")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(toggle-frame-maximized)
+
+(setq package-archives '(("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+          ("org"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+          ("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+
+(eval-when-compile
+  ;; Following line is not needed if use-package.el is in ~/.emacs.d
+  (add-to-list 'load-path "/Users/carlos/Documents/leju/APPS/emacs_carlos_personal/.emacs.d/use-package")
+  (require 'use-package))
+
+(use-package helm
+  :ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
